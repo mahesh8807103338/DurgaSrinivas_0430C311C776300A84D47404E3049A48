48c3cf5adee3884ceedd4d15bc0f5163847730d6
Mahesh8807103338 <126061147+mahesh8807103338@users.noreply.github.com>
2023-09-25 09:17:02 +0530

    Add files via upload

--- main (3).py
+++ main (3).py
@@ -1,13 +1,14 @@
-class Player:
-  def Play(self):
-    print("The player is playing cricket.")
-class Batsman(Player):
-  def play(self):
-    print("The player is batting.")
-class Bowler(Player):    
-  def play(self):
-    print("The bowler is bowling.")
-batsman= Batsman()
-bowler=Bowler()
-batsman.play()
-bowler.play()
+def linearSearchProduct(productList, targetProduct):
+  indices = []
+  for index,  product in enumerate(productList):
+    if product == targetProduct:
+      indices.append(index)
+
+  return indices
+
+
+products = ["shoes", "boot", "loafer", "shoes"]
+target = "shoes"
+target2 = 'apple'
+result = linearSearchProduct(products, target)
+print(result)
